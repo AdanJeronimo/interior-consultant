67459c00b92f3a9f2afaf161cead0a05e398731c
adan.jeronimo <adan22jeronimo@hotmail.com>
2023-08-15 15:50:33 -0600

    interior-consultant

---
+++ index.js
@@ -0,0 +1,10 @@
+// function mostrarMenu(){
+//     const menu = document.getElementById('navbar-reponsivo');
+//     if (menu.style.display === 'none') {
+//         menu.style.display = 'block';
+//     } else {
+//         menu.style.display = 'none';
+//     }
+    
+// }
+
